7742d04bdc4b054328bdfad53d33549b5df36441
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-21 21:57:30 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express'); // brings in the express library
+var app = express(); // starts the express app
 var bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+app.use(express.static('public')); // runs each time a request is made to look for static files in the public folder
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var songList = [
+var songList = [ // array of song objects
   {
     title: 'We did not start the Phire',
     artist: 'Billy Joel'
@@ -17,19 +17,19 @@ var songList = [
   }
 ];
 
-app.get('/songs', function(req, res) {
+app.get('/songs', function(req, res) { // this makes a request to get our songList array the the url /songs
   res.send(songList);
 });
 
-app.post('/newSong', function(req, res){
+app.post('/newSong', function(req, res){ // app.post adds information, but I can't remember exactly where it stores it.
   var newSong = req.body;
-  if(newSong.artist !== "Justin Bieber"){
+  if(newSong.artist !== "Justin Bieber"){ // conditional statement to either add the new song to the songList array or return an error to the user
     songList.push(newSong);
     console.log(songList);
-    res.sendStatus(200);
+    res.sendStatus(200); // notifies user of success
   } else {
-    res.sendStatus(500);
+    res.sendStatus(500); // notifies user of error
   }
 });
 
-app.listen(3000);
+app.listen(3000); // runs one time and then continues listening on port 3000
--- public/client.js
+++ public/client.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function(){ // ensures document is loaded on the DOM
   console.log('jquery was correctly sourced!');
-  getAllSongs();
-  function getAllSongs() {
-    $.ajax({
-      type: 'GET',
-      url: '/songs',
-      success: function(response) {
+  getAllSongs(); // calls the function, but what does this do?
+  function getAllSongs() { // creates a function, but what does this function do?
+    $.ajax({  // ajax get request. I'm not sure exactly what this is doing, though.
+      type: 'GET', // the type of request is 'GET'
+      url: '/songs', // refers to /songs url
+      success: function(response) { // if the request goes through, it will return a success or error message
         console.log('response', response);
       },
       error: function(error) {
@@ -14,24 +14,24 @@ $(document).ready(function(){
     });
   }
 
-  $('#addSongButton').on('click', function(){
-    var newSongTitle = $('#title').val();
-    var newSongArtist = $('#artist').val();
-    var newSongObject = {
+  $('#addSongButton').on('click', function(){ // button click event listener
+    var newSongTitle = $('#title').val(); // sets var to the title input value
+    var newSongArtist = $('#artist').val(); // sets var to the artist input value
+    var newSongObject = { // creating a new objet with two properties
       title: newSongTitle,
       artist: newSongArtist
     };
     console.log(newSongObject);
-    $.ajax({
-      type: 'POST',
-      url: '/newSong',
-      data: newSongObject,
-      success: function(response) {
+    $.ajax({ // second ajax request to add data
+      type: 'POST', // the request type 'POST' means that you are adding data
+      url: '/newSong', // not sure what this does
+      data: newSongObject, // not exactly sure what this does, but I assume it adds a new song object to the songsList array
+      success: function(response) {  // notify user if the POST request is successful
         console.log('response', response);
-        getAllSongs();
+        getAllSongs(); // call the function if the request is successful
       },
       error: function(error) {
-        console.log('error', error);
+        console.log('error', error); // notify the user if the POST request is not successful
       }
     });
   });
